29aeba9e72b5da565dab116c49da9fe898aa77f0
dj-idfx <35632851+dj-idfx@users.noreply.github.com>
2025-03-06 20:58:32 +0100

    Added PhpCsFixer action

--- src/Weight.php
+++ src/Weight.php
@@ -2,22 +2,22 @@
 
 namespace Idfx\WeightConversions;
 
-class Weight
-{
+use Pest\Plugins\Parallel;
+
+class Weight{
     protected float $kilograms;
 
-    public static function kilograms(float $kilograms): self
-    {
+    public static function kilograms(float $kilograms): self {
+        $myArray = array( 1 , 2, 3    , 4, 5, 6, 7, 8   , 9);
+
         return new static($kilograms);
     }
 
-    public function __construct(float $kilograms)
-    {
+    public function __construct(float $kilograms) {
         $this->kilograms = $kilograms;
     }
 
-    public function toPounds(): float
-    {
+    public function toPounds(): float {
         return $this->kilograms * 2.20462;
     }
 
